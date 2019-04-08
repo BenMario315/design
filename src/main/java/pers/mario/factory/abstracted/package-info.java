9d2572810e32be34145ec32fc6c4d3145ae303fe
mario <ben_mario@163.com>
2019-04-08 15:26:01 +0800

    abstract factory

---
+++ src/main/java/pers/mario/factory/abstracted/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @Project: design
+ * @PackageName: pers.mario.factory.abstracted
+ * @FileName: package-info.java
+ * @Description: The pers.mario.factory.abstracted is...
+ * @Author: mario
+ * @Time: 2019-04-08 15:24:18
+ * @Version:V1.0.0
+ */
+//抽象工厂模式
+package pers.mario.factory.abstracted;
